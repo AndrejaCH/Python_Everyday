59e4e7ddc12163ab47dfe53d5948c2be4fe09b94
Andreja Ho <sfandreja@gmail.com>
2021-01-15 09:36:27 -0800

    add code from day 3

--- 100DaysOfPython_Beginner.py
+++ 100DaysOfPython_Beginner.py
@@ -44,3 +44,39 @@ weeks = round(years * 52)
 days = round(years * 365)
 
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+####################################################################################
+# EVEN OR ODD NUMBER. CONDITIONALS, MODULO
+####################################################################################
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if number % 2 == 0:
+	print("This is even number.")
+else:
+	print("This is an odd number.")
+
+
+#######################################################################################
+# NESTED IF STATEMENT: BMI CALCULATOR
+#######################################################################################
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = weight / height ** 2
+
+if bmi < 18.5:
+	print(f"Your BMI is {bmi:.2f}, you are underweight.") 
+elif bmi >= 18.5 and bmi < 25:
+	print(f"Your BMI is {bmi:.2f}, you have a normal weight.")
+elif bmi >= 25 and bmi < 30:
+	print(f"Your BMI is {bmi:.2f}, you are slightly overweight.")
+else:
+	print(f"Your BMI is {bmi:.2f}, you are clinically obese.")
