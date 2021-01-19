c37da38a6cc8944867d33243a0674d3a5d65da6a
Andreja Ho <sfandreja@gmail.com>
2021-01-18 22:58:11 -0800

    fix bugs

--- Day4.py
+++ Day4.py
@@ -47,3 +47,36 @@ map[row -1][column -1] = "X"
 
 # 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
+
+################################################
+# Nested list
+#################################################
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen[1][1])
+# What will get printed?
+
+######################################################
+# financal roullete
+#########################################################
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
+
+
+
+#print(f"{random.choice(names)} will pay the bill")
