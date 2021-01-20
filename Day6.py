e52ebdbfbd1ddb6147039fb19903c54d12b8d49e
Andreja Ho <sfandreja@gmail.com>
2021-01-20 14:46:25 -0800

    add day5 activities

---
+++ Day6.py
@@ -0,0 +1,41 @@
+#################################################
+# FUNCTIONS AND WHILE LOOPS
+#################################################
+
+def my_function():
+    a = 3
+    if a > 2:
+    #This is my function
+        print("This will run")
+my_function()
+
+#########################################
+# WHILE LOOP AND CONDITION WITH FUNCTION
+##########################################
+while at_goal() == False:
+    jump()
+
+ while at_goal() != True:
+    jump()
+    
+while not at_goal():
+    jump()   
+
+########################################
+# wHILE LOOP
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -=1
+    print(number_of_hurdles)
+
+###################################
+# FOR LOOP AND LIST COMPREHAENSION
+# for loop   
+for x in range(6):
+    jump()
+# list comprehension
+find_flag = [jump() for x in range(6)]
+
+
+
---
+++ Day6_challenge.py
@@ -0,0 +1,30 @@
+###############################################
+## WHILE LOOPS AND FUNCTIONS
+##############################################
+
+## NOTES: CHECK THIS WHILE LOOP - WORKS AS IF/ELSE
+## SEE THE INDENTATION AT THE WHILE 
+## MAZE ROBOT PRACTICE: 
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+            move()
+    turn_left()
+
+while at_goal() == False:
+    if wall_in_front():
+            jump()
+    else:
+        move()
+
+##############################################################
+## MAZE CHALLENGE: DAY 6
+##############################################################
