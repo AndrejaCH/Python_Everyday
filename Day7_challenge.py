85348b3aa102fe6d3dfcbf8dd3e6c9d7d3d32faf
Andreja Ho <sfandreja@gmail.com>
2021-01-23 10:42:48 -0800

    add final code

--- Day7_challenge.py
+++ Day7_challenge.py
@@ -1,46 +1,42 @@
-#######################################################
-# HANGMAN
-########################################################
-
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_art import stages, logo
+from hangman_words import word_list
+#from replit import clear
+
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
+
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
-guess = input("Guess a letter: ").lower()
+for _ in range(word_length):
+    display += "_"
 
-for letter in chosen_word:
-	display.append('_')
-
-#print(display)
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-# Set the conditon for while loop to keep running
-end_of_game = False
-
-# Start while loop
-while end_of_game == False: 
+while not game_is_finished:
     guess = input("Guess a letter: ").lower()
-    for index, letter in enumerate(chosen_word):
+
+    #Use the clear() function imported from replit to clear the output between guesses.
+ 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
         if letter == guess:
-            display[index] = guess
+            display[position] = letter
+    print(f"{' '.join(display)}")
 
-    print(display)
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+    
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
 
-    # Set the condition that will end the while loop ("switch off the while loop")
-    if "_" not in display:
-        end_of_game = True
-        print("You win!")
-
-
-
-			
+    print(stages[lives])
