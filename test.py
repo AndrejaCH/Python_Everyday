926afb959171b8f749f0b5cc3dfd91548e7c5ba1
Andreja Ho <sfandreja@gmail.com>
2021-01-25 21:45:20 -0800

    add testing file

---
+++ test.py
@@ -0,0 +1,8 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+text = "hello"
+shift = 5
+
+for index, letter in enumerate(alphabet):
+	for t in text:
+		t = alphabet[index] + shift
+		print(t)
