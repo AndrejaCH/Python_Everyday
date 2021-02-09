0a8ffed655f631b3cfe9d0be747bff3da7832b89
Andreja Ho <sfandreja@gmail.com>
2021-02-08 18:04:57 -0800

    add find century and palidrome function

---
+++ CodeSignal_Practice/day1.py
@@ -0,0 +1,14 @@
+#Find a century
+# Find a century in a year
+def centuryFromYear(year):
+    return(year - 1) // 100 + 1
+
+print(centuryFromYear(2022))
+
+# Reverse string & Palindrome
+def checkPalindrome(inputString):
+    if inputString.lower() == inputString.lower()[::-1]:
+        return True
+    else:
+        return False
+    
