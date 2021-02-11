c00a949acb390db5e4a842241e7b9d6dbf73e290
Andreja Ho <sfandreja@gmail.com>
2021-02-10 22:35:11 -0800

    add leap year function

--- CodeSignal_Practice/leapYear_function.py
+++ CodeSignal_Practice/leapYear_function.py
@@ -35,3 +35,32 @@ string = ''.join(str(elem) for elem in [x + 1 for x in range(n)])
 print(string)
 
 ############################################################################################
+
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+	return random.choice(cards)
+
+print(deal_card())
+
+user_cards = []
+computer_cards = []
+
+for i in range(3):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+print(user_cards)
+print(computer_cards)
+
+
+def calculate_score(user_cards):
+	if user_cards == [10, 11] or user_cards == [11, 10]:
+		return 0
+	elif 11 in user_cards and sum(user_cards) > 21:
+		user_cards.remove(11) and user_cards.append(1)  
+    else:
+		return sum(user_cards)
+print(calculate_score(user_cards))
