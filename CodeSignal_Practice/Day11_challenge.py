857e27a5c484faf90ed2a5b8e1882fd83246c56d
Andreja Ho <sfandreja@gmail.com>
2021-02-12 16:07:14 -0800

    add codesignal activities

---
+++ CodeSignal_Practice/Day11_challenge.py
@@ -0,0 +1,26 @@
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+	return random.choice(cards)
+
+user_cards = []
+computer_cards = []
+
+for i in range(3):
+	user_cards.append(deal_card())
+	computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
+
+def calculate_score(user_cards):
+	if user_cards == [10, 11] or user_cards == [11, 10]:
+		return 0
+	elif 11 in user_cards and sum(user_cards) > 21:
+		user_cards.remove(11) and user_cards.append(1) and print(user_cards)
+	else:
+		return sum(user_cards) 
+
+print(calculate_score(user_cards))
